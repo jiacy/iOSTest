f415a5902352843964b7c9de06431bae5e4a063b
jiachenyu <jiachenyu@bytendance.com>
2018-11-18 12:15:52 +0800

    Initial Commit

---
+++ iOSTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOSTest
+//
+//  Created by jiacy on 2018/11/18.
+//  Copyright © 2018年 chneyuttest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iOSTest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSTest
+//
+//  Created by jiacy on 2018/11/18.
+//  Copyright © 2018年 chneyuttest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
